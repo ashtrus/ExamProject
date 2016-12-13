0eb656654b0a8e2d6861e5d06cecd50962ce44ae
Joana <m.joana2@gmail.com>
2016-12-14 00:05:09 +0100

    Creating admin via seed in migrations. Use "Update-Database -Verbose -Force" in PM

--- ExamProject/Migrations/Configuration.cs
+++ ExamProject/Migrations/Configuration.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace ExamProject.Migrations
 {
     using Microsoft.AspNet.Identity;
@@ -17,6 +19,7 @@ namespace ExamProject.Migrations
 
         protected override void Seed(ExamProject.Models.ApplicationDbContext context)
         {
+            // https://code.msdn.microsoft.com/ASPNET-MVC-5-Security-And-44cbdb97
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
@@ -30,9 +33,64 @@ namespace ExamProject.Migrations
             //    );
             //
             var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
-           //userManager.Create
+            //userManager.Create
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            if (!roleManager.RoleExists("Admin"))
+            {
+
+                // first we create Admin rool   
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+                //Here we create a Admin super user who will maintain the website                  
+
+                if (!userManager.Users.Any(x => x.UserName == "admin@kea.dk"))
+                {
+                    var user = new ApplicationUser { UserName = "admin@kea.dk", Email = "admin@kea.dk" };
+
+                    string userPWD = "123321";
+
+                    var chkUser = userManager.Create(user, userPWD);
+
+                    //Add default User to Role Admin   
+                    if (chkUser.Succeeded)
+                    {
+                        var result1 = userManager.AddToRole(user.Id, "Admin");
+                    }
+                }
+
+            }
 
 
+            // creating Creating Employee role    
+            if (!roleManager.RoleExists("Employee"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Employee";
+                roleManager.Create(role);
+
+            }
+
+            // 
+            if (!userManager.Users.Any(x => x.UserName == "admin@kea.dk"))
+            {
+                var user = new ApplicationUser { UserName = "admin@kea.dk", Email = "admin@kea.dk" };
+
+                string userPWD = "123321";
+
+                var chkUser = userManager.Create(user, userPWD);
+
+                //Add default User to Role Admin   
+                if (chkUser.Succeeded)
+                {
+                    var result1 = userManager.AddToRole(user.Id, "Admin");
+                }
+            }
+
+            var adminUser = userManager.Users.First(x => x.UserName == "admin@kea.dk");
+            Debug.WriteLine($"{adminUser.UserName} exists");
         }
        
     }
--- ExamProject/Models/Role.cs
+++ ExamProject/Models/Role.cs
@@ -8,10 +8,10 @@ namespace ExamProject.Models
     public class Role
     {
         public int RoleId { get; set; } //Primary Key 
-        public int UserId { get; set; } //User 
+        //public int UserId { get; set; } //User 
         public string Title { get; set; } // title 
         public string Description { get; set; } //Desc
-        public int Experience { get; set; }  // number of years
+        //public int Experience { get; set; }  // number of years
         public DateTime SelectedOn { get; set; }  // date when role was selected
         public virtual List<Tag> Tags { get; set; } // to group similar roles
         public virtual List<string> Curriculum { get; set; } // wikipedia links, books, youtube/vimeo videos
